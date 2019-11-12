5f4fd2258becaddff8070e561cf1dcd4e932bf96
Emma Vestesson <emma.vestesson@gmail.com>
2019-11-12 22:10:42 +0000

    updated theme

--- Blog set up.R
+++ Blog set up.R
@@ -1,6 +1,11 @@
 library(blogdown)
 
 serve_site()
+build_site()
 new_post('Reaching for the stars',  ext = '.Rmd')
 new_post('useR2019!',  ext = '.Rmd')
 new_post('Function of the week: uncount',  ext = '.Rmd', tags = c('tidyr', 'uncount'))
+
+
+install_theme('tranquilpeak', hostname = "kakawait/hugo-tranquilpeak-theme", theme_example = FALSE, 
+              update_config = TRUE, force = TRUE, update_hugo = FALSE)
